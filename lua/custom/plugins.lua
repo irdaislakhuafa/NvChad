bffc1ecff2c45b5c8b3ec57d79d75d116d8b566f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-28 05:00:26 +0700

    added jparise/vim-graphql for GraphQL support

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,18 +1,4 @@
 local plugins = {
-  -- [DISABLED] vs code color scheme for neovim
-  -- {
-  --   "Mofiqul/vscode.nvim",
-  --   config = function ()
-  --     -- for dark theme, use "light" for light theme
-  --     vim.o.background = "dark"
-
-  --     local opts = require("custom.configs.vscode-conf")
-  --     local vscode = require("vscode")
-  --     vscode.setup(opts)
-  --     vscode.load()
-  --   end
-  -- }
-  
   -- Mason
   {
     "williamboman/mason.nvim",
@@ -75,6 +61,11 @@ local plugins = {
     build = function ()
       vim.cmd([[silent! GoInstallDeps]])
     end
+  },
+
+  -- GraphQL for vim
+  {
+    "jparise/vim-graphql",
   },
 }
 return plugins
