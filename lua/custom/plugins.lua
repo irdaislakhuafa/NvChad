52bba0ff2bc1b130686dbe83a75fec5e3745f4f2
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-28 01:10:04 +0700

    added simple custom plugins

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,22 @@
+local plugins = {
+  -- [DISABLED] vs code color scheme for neovim
+  -- {
+  --   "Mofiqul/vscode.nvim",
+  --   config = function ()
+  --     -- for dark theme, use "light" for light theme
+  --     vim.o.background = "dark"
+
+  --     local opts = require("custom.configs.vscode-conf")
+  --     local vscode = require("vscode")
+  --     vscode.setup(opts)
+  --     vscode.load()
+  --   end
+  -- }
+  {
+    "junegunn/fzf.vim",
+    config = function ()
+      require("fzf").setup()
+    end
+  },
+}
+return plugins
