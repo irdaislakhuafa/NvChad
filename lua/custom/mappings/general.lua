fe244389c0cc41b06da1a497e6c302eea317eb51
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-28 13:21:35 +0700

    fix buftype

--- lua/custom/mappings/general.lua
+++ lua/custom/mappings/general.lua
@@ -1,8 +1,9 @@
 local keymaps = {
   i = {
-    ["<C-s>"] = { "<ESC>:w <CR>", "Write/Save file" },
+    ["<C-s>"] = { "<ESC>:set buftype=\"\" | :w <CR>", "Write/Save file", { silent = true } },
   },
   n = {
+    ["<C-s>"] = { "<ESC>:set buftype=\"\" | :w <CR>", "Write/Save file", { silent = true } },
     ["<A-t>"] = { ":Telescope <CR>", "Telescope toggle", { silent = true } },
     ["<C-l>"] = { ":NvimTreeToggle <CR>", "Telescope toggle", { silent = true } },
     ["<leader>cc"] = { ":Telescope commands <CR>", "VIM commands with Telescope", { silent = true } },
