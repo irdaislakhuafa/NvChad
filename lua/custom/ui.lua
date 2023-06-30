9cd1c763ccf4e3c4960955fe679005a4c65468ef
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-30 22:04:18 +0700

    added my custom ui

--- lua/custom/ui.lua
+++ lua/custom/ui.lua
@@ -1,14 +1,16 @@
 local ui = {
   theme = "nord",
+  theme_toggle = { "nord", "github_light" },
   changed_themes = {
     nord = {
       base_16 = {
-        -- base00 = "#2c3e50",
+        base00 = "#002b36",
       },
       base_30 = {
-        -- black = "#2c3e50",
-        -- black2 = "#283d5e",
-        -- darker_black = "#2c3e55", -- nvim-tree background
+        black = "#002b36",
+        black2 = "#06313c",
+        darker_black = "#002530", -- nvim-tree background
+        statusline_bg = "#042f3a",
       },
     },
   },
@@ -23,6 +25,30 @@ local ui = {
     CursorColumn = {
       bg = "one_bg",
     },
+  },
+  statusline = {
+    separator_style = "arrow",
+  },
+  nvdash = {
+    load_on_startup = true,
+    buttons = {
+      { "      { Irda Islakhu Afa }", "", "" },
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "🔖 Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+  cheatsheet = {
+    theme = "grid",
+  },
+  telescope = {
+    style = "bordered",
+  },
+  tabufline = {
+    show_numbers = true,
   },
 }
 
