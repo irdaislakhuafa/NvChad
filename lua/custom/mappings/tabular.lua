fc99fc99718cde60a07b00e5c84998d949ec9ef2
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-01 15:22:59 +0700

    added shortcut for Tabular.vim

--- lua/custom/mappings/tabular.lua
+++ lua/custom/mappings/tabular.lua
@@ -3,5 +3,8 @@ local keymaps = {
   n = {
     ["<leader>tab"] = { ":Tabularize /", "Align text with Tabular.vim", { silent = false } },
   },
+  v = {
+    ["<leader>tab"] = { ":Tabularize /", "Align text with Tabular.vim", { silent = false } },
+  },
 }
 return keymaps
